38833cc9009487e1e3d76c6de3f664bc9a8a0918
Jonah Birch <BircJona466@GoTVDSB.ca>
2021-10-06 13:57:54 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Loops___Random_Updated_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[25];
+            Random rnd = new Random();
+            int min;
+            int max;
+
+            min = Convert.ToInt32(Console.ReadLine());
+            max = Convert.ToInt32(Console.ReadLine());
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                numbers[i] = rnd.Next(min, max);
+                Console.WriteLine(numbers[i]);
+
+            }
+
+
+            Console.WriteLine("Press ENTER to Close.....");
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
